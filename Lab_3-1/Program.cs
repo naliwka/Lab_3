fa537da75d72f0a225d0e543d5f42c6f84fb08ce
naliwka <avierina.nataliia@gmail.com>
2022-10-04 21:51:14 +0300

    Lab_3-1

--- Lab_3-1/Program.cs
+++ Lab_3-1/Program.cs
@@ -10,6 +10,18 @@ namespace Lab_3_1
     {
         static void Main(string[] args)
         {
+            double side1, side2;
+            Console.WriteLine("Enter the first side:");
+            side1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the second side:");
+            side2 = Convert.ToDouble(Console.ReadLine());
+
+            Rectangle rectangle = new Rectangle(side1, side2);
+            double area = rectangle.Area;
+            double perimeter = rectangle.Perimeter;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Area = {area}\nPerimeter = {perimeter}");
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
--- Lab_3-1/Rectangle.cs
+++ Lab_3-1/Rectangle.cs
@@ -8,5 +8,35 @@ namespace Lab_3_1
 {
     internal class Rectangle
     {
+        private double side1;
+        private double side2;
+        public double Area
+        {
+            get
+            {
+                return AreaCalculator();
+            }
+        }
+        public double Perimeter
+        {
+            get
+            {
+                return PerimeterCalculator();
+            }
+        }
+        public Rectangle(double side1, double side2)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+        public double AreaCalculator()
+        {
+            return side1 * side2;
+        }
+        public double PerimeterCalculator()
+        {
+            return (side1 + side2) * 2;
+        }
+        
     }
 }
